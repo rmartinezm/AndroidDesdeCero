42a5a956965f483d7d83f870e40b677c2f212951
rmartinezm <robertomtz@ciencias.unam.mx>
2017-07-02 01:54:36 -0500

    finished

--- app/src/main/java/com/programacion/robertomtz/androiddesdecero/MainActivity.java
+++ app/src/main/java/com/programacion/robertomtz/androiddesdecero/MainActivity.java
@@ -9,22 +9,61 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Declaremos nuestro EditText para poderlo utilizar dentro de toda nuestra Clase
+     */
     EditText etPassword;
 
+    /**
+     * El Método onCreate es el primero en ejecutarse cuando abrimos nuestra ventana
+     * Es común que en este método asignemos los valores a nuestras variables
+     */
+    // @Override es una etiqueta que por el momento no vale la pena aprender
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Las dos siguientes lineas se crean automaticamente cuando es creado el Activity.
+        // Es mejor no moverlas y no escribir instrucciones antes de estas dos lineas.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // findViewById(R.id.miIdentificador) es la forma en que vinculamos un View de nuestro .xml
+        // con un Objeto en Java.
+        // Colocamos antes de esta funcion entre parentesis el Objeto que guardaremos en nuestra
+        // variable.
 
         etPassword = (EditText) findViewById(R.id.et_password);
     }
 
-    public void metodoEntrar(View v){
+    /**
+     * Este metodo se ejecutara cuando el botón en que tengamos la propiedad
+     * android:onClick="metodoEntrar" sea precionado
+     * El parametro view: Se pone obligatoriamente por razones no tan importantes.
+     */
+    public void metodoEntrar(View view){
+        // El método getText() de un TextView sirve para extraer el texto introducido en él.
+        // El método toString() es obligatorio para convertir el texto a un String
         String password = etPassword.getText().toString();
-        if (password.equals("313087194")){
+
+        // El método equals("otro String") sirve para verificar si una cadena de texto es igual
+        // a otra. (Para los String no es recomendable utilizar la comparacion de numeros == )
+        if (password.equals("123456")){
+
+            // El objeto Intent es el que nos sirve para movernos dentro de la aplicacion.
+            // No solo sirve para cambiar de pantalla, tambien podriamos abrir la camara, la galeria,
+            // una pagina de internet, etc. pero con otra sintaxis.
+            // Como parametros recibe la palabra reservada 'this' además del nombre de la clase
+            // a la que queremos ir seguida de .class
             Intent intent = new Intent(this, MusicaActivity.class);
+
+            // Con este método ejecutamos el intent para cambiar de pantalla
             startActivity(intent);
         }else{
+            // Toast es un Objeto que nos proporciona android para mostrar informacion a nuestro
+            // usuario. Se utiliza para mensajes cortos cuando ocurre algo dentro de la aplicacion.
+            // Para crear un Toast es necesario seguir esta sintaxis.
+            // Recibe como primer parametro la palabra 'this' seguido del texto que queremos mostrar
+            // y por ultimo las palabras Toast.LENGTH_SHORT si queremos que el Toast se muestre
+            // poco tiempo o bien Toast.LENGTH_LONG si queremos que se muestre un poco mas tiempo.
             Toast.makeText(this, "Contraseña erronea!", Toast.LENGTH_SHORT).show();
         }
     }
--- app/src/main/java/com/programacion/robertomtz/androiddesdecero/MusicaActivity.java
+++ app/src/main/java/com/programacion/robertomtz/androiddesdecero/MusicaActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 
 public class MusicaActivity extends AppCompatActivity {
 
+    /**
+     * Declaramos nuestro Objeto MediaPlayer con el que reproduciremos nuestras canciones
+     */
     MediaPlayer miReproductorMusical;
 
     @Override
@@ -14,14 +17,35 @@ public class MusicaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_musica);
 
+        // La palabra reservada 'null' es de gran importancia en la programación orientada a
+        // Objetos, con ella estamos diciendo que nuestro Objeto es nulo, o que no existe.
+        // Con esta instruccion estamos diciendo que nuestro reproductorMusical aún no existe, pero
+        // que lo crearemos en algun momento de nuestra aplicacion.
         miReproductorMusical = null;
     }
 
+    /**
+     * Metodo que se ejecutara al dar click sobre la imagen que tiene el metodo onClick con este metodo
+     */
     public void metodoPxndx(View v){
+        // Esta condicion es necesaria para que la aplicacion pueda detener el reproductor de musica
+        // si este ya fue creado anteriormente.
+        // Si no colocaramos la condicional 'miReproductorMusical != null' la aplicacion querra
+        // detener algo que no existe por el momento y eso haria que la aplicación se detuviera
         if (miReproductorMusical != null) {
             miReproductorMusical.stop();
         }
+
+        // Esta es una forma muy común de crear objetos en Java sin tener que llamar a la palabra
+        // recervada 'new' Se coloca el nombre de la Clase del que queremos el Objeto seguido de
+        // la intruccion '.create' con el parametro this y las palabras R.raw.nombre_cancion
+        // para hacer referencia a la cancion en nuestra carpeta raw.
+        // No todas las Clases tienen la posibilidad de crear Objetos de esta manera.
+        // Nosotros tenemos que crearlos. (Quzá en el curso Android Básico a Intermedio lo veamos)
         miReproductorMusical = MediaPlayer.create(this, R.raw.pxndx_cancion);
+
+        // Con el método start() iniciamos la canción que pasamos como parametro en la creación
+        // del Objeto.
         miReproductorMusical.start();
     }
 
@@ -49,9 +73,20 @@ public class MusicaActivity extends AppCompatActivity {
         miReproductorMusical.start();
     }
 
+    /**
+     * El Método onStop() se ejecuta cuando la pantalla fue detenida de alguna forma.
+     * Puede ser que la aplicación se haya minimizado o que el telefono fue bloqueado, así que dentro
+     * de éste metodo pondremos las acciones que necesitamos que se realicen antes de que
+     * la aplicacion se detenga.
+     * Las palabras super.onStrop() son necesarias para que la aplicacion pueda minimizarse de
+     * la forma correcta. siempre tiene que ir al final de nuestro método.
+     */
     @Override
     protected void onStop() {
+        // Hacemos la misma condicional para que la aplicacion no se detenga si solo queremos
+        // detener la musica
         if (miReproductorMusical != null) {
+            // Con el metodo stop() hacemos que la música se detenga.
             miReproductorMusical.stop();
         }
         super.onStop();
